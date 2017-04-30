5196c14d9095502310dcee11b4a1601f19896954
Patrick Hussey <husspm@gmail.com>
2017-04-30 18:25:37 -0400

    finishing touches

--- friends.js
+++ friends.js
@@ -1,31 +1,31 @@
 exports.friends = [{
         name: "Patrick",
-        photo: "fkjkdjf",
+        photo: "http://speakingaboutpresenting.com/wp-content/uploads/2009/01/istock_businessman.jpg",
         score: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
     },
     {
         name: "Emily",
-        photo: "sdjkjsd",
+        photo: "http://speakingaboutpresenting.com/wp-content/uploads/2009/01/younger-business-woman.jpg",
         score: [5, 5, 4, 5, 5, 4, 5, 5, 4, 5]
     },
     {
         name: "Chester",
-        photo: "sjdksjd",
+        photo: "http://static6.businessinsider.com/image/4fbceec36bb3f7461000000b/sorry-but-people-who-lost-money-on-facebook-stock-have-only-themselves-and-their-advisors-to-blame.jpg",
         score: [2, 4, 2, 3, 5, 4, 3, 2, 1, 3]
     },
     {
         name: "Sarah",
-        photo: "fgkjkfjg",
+        photo: "http://www.photl.com/www3/photos/2010/01/04/2233/wm232865tt.jpg",
         score: [2, 3, 2, 4, 5, 4, 3, 2, 3, 4]
     },
     {
         name: "Leo",
-        photo: "dkjjkdf",
+        photo: "https://pbs.twimg.com/media/CynmmdYWgAAjky1.jpg",
         score: [2, 2, 3, 3, 2, 2, 5, 5, 4, 4]
     },
     {
         name: "Chelsea",
-        photo: "kslkllk",
+        photo: "http://img02.deviantart.net/3937/i/2007/006/f/a/scared_people_3_by_b_e_c_k_y_stock.jpg",
         score: [1, 2, 5, 4, 5, 4, 3, 2, 3, 4]
     }
 ];
